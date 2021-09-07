836a66090a37370ebdd4f29675fe41b8fde5461b
satoyoshi44 <satoyoshi44@gmail.com>
2021-09-07 19:48:48 +0900

    add new file

---
+++ script.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.slide-wrap').slick({
+        accessibility: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+        fade: true,
+    });
+});
